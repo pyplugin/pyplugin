9e70ef8f86fe267dae86e50ac8411c9f722efd4b
SirRender00 <evyn.machi@gmail.com>
2023-09-22 15:57:07 -0700

    feat: add lookup_plugin

--- pyplugin/__init__.py
+++ pyplugin/__init__.py
@@ -1,3 +1,3 @@
-from pyplugin.base import Plugin, register, unregister, get_registered_plugin, get_plugin_name
+from pyplugin.base import Plugin, register, unregister, get_registered_plugin, get_plugin_name, lookup_plugin
 from pyplugin.decorators import plugin
 from pyplugin.types import PluginLike
