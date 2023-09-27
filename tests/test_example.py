4b0bced8b651553232a5d6c5d785d99bb948a1ab
SirRender00 <evyn.machi@gmail.com>
2023-09-26 18:42:06 -0700

    test: Add simple requirement test suite

--- tests/test_example.py
+++ tests/test_example.py
@@ -3,16 +3,19 @@ from pyplugin import plugin
 
 @plugin
 def upstream(arg=4):
-    print(arg)
     return arg
 
 
 @plugin(requires="tests.test_example.upstream")
 def two_plus_two(**kwargs):
-    print(kwargs)
     return kwargs
 
 
-two_plus_two()
+def test_example():
+    answer = 4
+    assert two_plus_two()["upstream"] == answer
 
-upstream(5)
+    answer = 5
+    upstream(answer)
+
+    assert two_plus_two.instance["upstream"] == answer
